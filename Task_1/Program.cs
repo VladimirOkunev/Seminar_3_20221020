3ea0375e77eb5fd3120bbfb463b250f8df8c2977
Vladimir_Okunev <vova-okunev@mail.ru>
2022-10-21 15:37:23 +0900

    added task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Enter three digits number: ");
+
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+
+while (number < 100 || number > 999) // check the correctness of the value
+{
+    Console.Clear();
+    Console.WriteLine("Incorrect value");
+    Console.Write("Please, enter again three digits number:  ");
+    number = int.Parse(Console.ReadLine() ?? "0");
+}
+
+number = number/10;
+int secondDigit = number%10;
+Console.Write($"The third digit of the number:  {secondDigit}");
+
